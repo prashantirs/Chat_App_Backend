ebf0ac0da0a683dc4b1a4a13a34701dfcbdec13b
Prashant Srivastava <prashantsrivastava381@gmail.com>
2023-07-02 17:25:39 +0530

    backend made of chat app

---
+++ server.js
@@ -0,0 +1,49 @@
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const socketIo = require('socket.io');
+
+const port = process.env.PORT || 4500;
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = socketIo(server);
+const users = [{}]
+
+io.on('connection', (socket ) => {
+    console.log('New client connected');
+    
+    //time
+    const dateString = socket.handshake.time;
+    const date = new Date(dateString);
+    const options = { hour: '2-digit', minute: '2-digit', hour12: true };
+    const time = date.toLocaleTimeString([], options);
+
+    socket.on('joined',({name})=>{
+        users[socket.id] = name;
+        // socket.emit('welcome',{message:`${name} Welcome to the chat at ${time}`});
+        socket.emit('welcome',{user: "Admin",text:`${name} Welcome to the chat`,id:socket.id});
+        // socket.broadcast.emit('newUser',{message:`${name} joined the chat at ${time}`});
+        socket.broadcast.emit('newUser',{user: 'Admin',text:`${name}joined the chat`,id:socket.id});
+    })
+
+    socket.on('disconnect', () => {
+        console.log(`${users[socket.id]} left`);
+        // socket.broadcast.emit('userLeft',{message:`${users[socket.id]} left the chat at ${time}`});
+        socket.broadcast.emit('userLeft',{user: "Admin",text:`${users[socket.id]} left the chat`,id:socket.id});
+        
+    });
+
+    socket.on('message',({text , id})=>{
+        io.emit('sendMessage',{user: users[id],text,id});
+    })
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+server.listen(port, () => {
+    console.log(`Server is running on  http://localhost:${port}`);
+});
